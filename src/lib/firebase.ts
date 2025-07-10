78d88261bde427d7afe44d3ddcf2180e262f2716
Abdulloh <abdulloh50007@gmail.com>
2025-07-10 12:40:12 +0000

    создай env file и там сохраняй данные из firestore

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,12 +9,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "REDACTED_FIREBASE_API_KEY",
-  authDomain: "REDACTED_FIREBASE_AUTH_DOMAIN",
-  projectId: "REDACTED_FIREBASE_PROJECT_ID",
-  storageBucket: "REDACTED_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "REDACTED_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "REDACTED_FIREBASE_APP_ID"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
