9ad22cd9b26e3705620463e7e823ebe2befeeb35
Abdulloh <abdulloh50007@gmail.com>
2025-07-13 17:42:44 +0000

    finalazing after 17 hours of working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body className={cn('font-body antialiased min-h-screen flex flex-col')}>
         <ThemeProvider
