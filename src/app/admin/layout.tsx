a8a22280c7ee36653b7d0d7beda8f1b4f40756cc
Abdulloh <abdulloh50007@gmail.com>
2025-08-08 12:27:42 +0000

    V 6.1.0

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,5 +2,5 @@ import { AuthProvider } from '@/contexts/auth-context';
 import { ReactNode } from 'react';
 
 export default function AdminRootLayout({ children }: { children: ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
+  return <AuthProvider>{children}</AuthProvider>; 
 }
